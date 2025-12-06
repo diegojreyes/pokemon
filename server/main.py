ac7ef653a8cb539f2a2b1cb418e269f170eb571b
diegojreyes <diego.reyes4@upr.edu>
2025-12-06 06:54:10 +0000

    dep

--- server/main.py
+++ server/main.py
@@ -31,7 +31,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", 
                    "http://127.0.0.1:5173",
-                   ],  # Add 127.0.0.1
+                   "https://pokemon-static-p77n.onrender.com"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
