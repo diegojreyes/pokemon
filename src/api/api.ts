57bfcc94a6514b4128220753e1e519206d6d1eb6
diegojreyes <diego.reyes4@upr.edu>
2025-12-06 06:50:58 +0000

    dp

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL : "http://127.0.0.1:8000/"
+    baseURL : import.meta.env.SERVER ? import.meta.env.SERVER : "http://127.0.0.1:8000/"
 })
 
 export default api
